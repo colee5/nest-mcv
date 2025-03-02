92274581efe85d7fb514dae3cda1787feb510f20
Oliver Gjorgjiev <oliver.gorgiev11@gmail.com>
2025-03-02 21:34:04 +0100

    lt

--- leet-python.py
+++ leet-python.py
@@ -7,3 +7,61 @@ class Solution:
                 nums[k] = nums[i]
                 k += 1
         return k;
+
+
+class Solution2:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        ans = []
+
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
+            return ans
+        
+
+
+class Solution3:
+    def isValid(s):
+        stack = []
+        closeToOpen = {')': '(',']': '[','}': '{',};
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.push(c)
+        return True if not stack else False;
+        
+    
+
+class ListNode:
+    
+    def __init__(self, val, next_node=None):
+        self.val = val
+        self.next = next_node
+    
+class Solution3:
+
+    def __init__(self):
+        # Dummy Node - which makes the removing a node 
+        # form the beginning of the list easier.
+        self.head = ListNode(-1)
+        self.tail
+
+    def get(self, index:int) -> int:
+        curr = self.head.next
+        i = 0
+
+        while curr:
+            if i == index:
+                return curr.val
+            i += 1
+            curr = curr.next
+
+    def insertHead(self, val:int) -> None:           
+        new_node = ListNode(val)
+        new_node.next = self.head.next
+        self.head.next = new_node 
--- leet-typescript.ts
+++ leet-typescript.ts
@@ -13,6 +13,8 @@ const removeDuplicates = (nums: number[]) => {
   return l;
 };
 
+//
+
 const removeElement = (nums: number[], val: number) => {
   let k = 0;
   for (let i = 0; i < nums.length; i++) {
@@ -23,3 +25,103 @@ const removeElement = (nums: number[], val: number) => {
   }
   return k;
 };
+
+//
+
+const concatElement = (nums: number[]) => {
+  let n = nums.length;
+  let ans = new Array(n * 2);
+
+  for (let i = 0; i < n; i++) {
+    ans[i] = nums[i];
+    ans[i + n] = nums[i];
+  }
+
+  return ans;
+};
+
+//
+
+const isValid = (s: string): boolean => {
+  let stack: string[] = [];
+  const closeToOpen = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
+
+  for (let c of s) {
+    if (closeToOpen[c]) {
+      if (stack.length > 0 && stack[stack.length - 1] === closeToOpen[c]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(c);
+    }
+  }
+
+  return stack.length === 0;
+};
+
+//
+
+class MinStack {
+  stack: number[];
+  minStack: number[];
+
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(val: number): void {
+    if (this.stack.length > 0) this.stack.push(val);
+
+    if (
+      this.minStack.length === 0 ||
+      val <= this.minStack[this.minStack.length - 1]
+    ) {
+      this.minStack.push(val);
+    }
+  }
+
+  pop(): void {
+    const popped = this.stack.pop();
+    if (popped == this.minStack[this.minStack.length - 1]) this.minStack.pop();
+  }
+
+  top(): number | undefined {
+    if (this.stack.length > 0) return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
+
+//
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let previous: ListNode | null = null;
+  let current = head;
+
+  while (current) {
+    let nextNode = current.next;
+    current.next = previous;
+
+    previous = current;
+    current = nextNode;
+  }
+  return previous;
+}
